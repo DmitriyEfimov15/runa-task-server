79366f648651ee059d343176358ba9b6841b3b4a
Dmitry <a.d1mon@yandex.ru>
2026-01-10 19:57:42 +0300

    test(server): пути перекинуты в schema.prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'prisma/config';
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-console.log('SHADOW_DATABASE_URL:', process.env.SHADOW_DATABASE_URL);
-
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
