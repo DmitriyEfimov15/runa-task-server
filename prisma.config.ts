3f297b04b5e3363f582f6cdd75c7e674c67882d4
Dmitry <a.d1mon@yandex.ru>
2026-01-10 19:34:52 +0300

    test(server): проверка работы prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'prisma/config';
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+console.log('SHADOW_DATABASE_URL:', process.env.SHADOW_DATABASE_URL);
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
